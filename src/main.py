0b3d40d9a795c7de61c529c28e6d934b0d9fa255
Maks K <157726925+VocalSoda@users.noreply.github.com>
2026-09-19 10:49:33 +0300

    - score mechanics have been added

--- src/main.py
+++ src/main.py
@@ -4,8 +4,43 @@
 
 import random
 from inputimeout import inputimeout, TimeoutOccurred
+import time
+import math
 
-db_fetch_result = (["aAa", "fi"], ["bbb", "de"], ["ccC", "fi"], ["DDD", "de"], ["565", "fi"], ["343", "de"], ["2934", "fi"], ["326", "de"], ["944", "fi"], ["4", "de"], ["5", "fi"], ["45345", "de"]) #Example dataset used for basic debugging and testing
+db_fetch_result = (
+    ["aAa", "fi"],
+    ["bbb", "de"],
+    ["ccC", "fi"],
+    ["DDD", "de"],
+    ["565", "fi"],
+    ["343", "de"],
+    ["2934", "fi"],
+    ["326", "de"],
+    ["944", "fi"],
+    ["4", "de"],
+    ["5", "fi"],
+    ["45345", "de"],
+    ["ABC1", "se"],
+    ["DEF2", "no"],
+    ["GHI3", "dk"],
+    ["JKL4", "fr"],
+    ["MNO5", "es"],
+    ["PQR6", "it"],
+    ["STU7", "nl"],
+    ["VWX8", "pl"],
+    ["YZA9", "cz"],
+    ["BCD0", "ee"],
+    ["EFG1", "lv"],
+    ["HIJ2", "lt"],
+    ["KLM3", "is"],
+    ["NOP4", "ie"],
+    ["QRS5", "pt"],
+    ["TUV6", "gr"],
+    ["WXY7", "ch"],
+    ["ZAB8", "at"],
+    ["CDE9", "be"],
+    ["FGH0", "lu"],
+)
 fetch_to_list=list(db_fetch_result) #converting tuple into a list as SQL db output is a tuple
 
 
@@ -15,29 +50,29 @@ index_list = []
 for index, item in enumerate(fetch_to_list):
     index_list.append(index)
 
-fetch_result = []
 
-        
-while len(fetch_result) < 7:
+
+def generate_fetch_result():
+    fetch_result = []
+
+    while len(fetch_result) < 7:
         rand_index = random.choice(index_list)
-        try:
-                fetch_result.append(fetch_to_list[rand_index])
-                index_list.remove(rand_index)
-       
-        except:
-            pass
-        
-        
-  
-for item in fetch_result:
-        if random.randrange(1, 4) % 2 == 0:
-            item.append(True)
-        else:
-            item.append(False)
+        index_list.remove(rand_index)
 
+        fetch_result.append(fetch_to_list[rand_index])
 
-  
+   
+    for item in fetch_result:
+        item.append(random.randrange(1, 4) % 2 == 0)
 
+    
+    if not any(item[2]==True for item in fetch_result):
+        rand_index = random.randint(0, len(fetch_result)-1)
+        fetch_result[rand_index][2] = True
+
+    return fetch_result
+
+fetch_result = generate_fetch_result()
 
 #core game variables
 round_count = 1
@@ -57,12 +92,14 @@ while True:
             icao_list.append(item[0])
     
    
-    
+    start_time = time.perf_counter()
     try:
         usr_input = inputimeout(prompt = "Please type in ICAO code: ", timeout = 10)
     except TimeoutOccurred:
         print("Your time is up")
         break
+    
+    elapsed_time = time.perf_counter() - start_time
     
     if usr_input in icao_list:
         
@@ -75,7 +112,8 @@ while True:
                fetch_result.pop(index_check)
         icao_list = [item[0] for item in fetch_result if item[2]]
                
-        player_score = player_score+1
+        print(f"Elapsed time: {elapsed_time}")
+        player_score = player_score + math.floor(elapsed_time)        
         round_count = round_count +1                   
     else:
         print('\n'+"Game Over"+'\n')
@@ -84,6 +122,13 @@ while True:
     # print(fetch_result)
     # print(icao_list)
     
-    if len(icao_list) == 0 or round_count == 5:
+    if len(icao_list) == 0:
+        fetch_result = generate_fetch_result()
+
+
+        
+         
+    
+    if round_count == 6:
         print("Victory"+'\n')
         break
